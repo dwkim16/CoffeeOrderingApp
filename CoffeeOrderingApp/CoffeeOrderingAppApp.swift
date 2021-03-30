f6ed26751afbfdfaaa7d8f07d6bfbfd9e8c5cd95
Dowon Kim <dwkim16@gmail.com>
2021-03-30 21:19:39 +0900

    Initial Commit

---
+++ CoffeeOrderingApp/CoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingAppApp.swift
+//  CoffeeOrderingApp
+//
+//  Created by 김도원 on 2021/03/30.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
